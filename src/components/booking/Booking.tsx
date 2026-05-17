51d2b6b3626cf3cbafb037883472be1d9367fce8
Le Tan <tanle782005@gmail.com>
2026-05-17 21:29:01 +0700

    style(booking): customize DayPicker UI

--- src/components/booking/Booking.tsx
+++ src/components/booking/Booking.tsx
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
 
 export const Booking = () => {
   const [range, setRange] = useState<any>({});
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
   return (
-    <div className="grid grid-cols-2 border border-primary-800">
-      <div className="flex flex-row justify-between  text-white rounded-2xl overflow-hidden shadow-2xl">
+    <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] bg-[#0f1c2e] text-white rounded-2xl overflow-hidden shadow-2xl">
+      <div className="p-6 flex flex-col  justify-between">
         <DayPicker
           animate
           mode="range"
           selected={range}
           onSelect={setRange}
           numberOfMonths={2}
+          disabled={{ before: today }}
           className="text-white rdp-custom"
           styles={{
-            day: { borderRadius: "8px" },
+            day: { borderRadius: "10px" },
           }}
         />
       </div>
 
-      <div>sdg</div>
+      <div className="bg-[#1e293b] p-8 flex flex-col justify-between">sdg</div>
     </div>
   );
 };
